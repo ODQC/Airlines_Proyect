949258e30cb801f3780852ff22f7a458ca6b7ee6
Oscar Quesada <oscaardanielqc@outlook.es>
2020-09-16 15:21:12 -0600

    Implementación de MVC
    Se implementó los modelos, vistas y controladores y las interfaces en el proyecto

---
+++ Proyecto1_PrograIII/src/com/progra/una/controlador/Validaciones.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progra.una.controlador;
+
+/**
+ *
+ * @author oscardanielquesadacalderon
+ */
+public class Validaciones {
+    
+}
